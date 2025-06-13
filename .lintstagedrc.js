79c6e0f8b3a5da1ab21303b9fe84df8b6c9ac5b4
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-06-13 15:46:34 -0400

    Update lint-staged path

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,11 @@
-const path = require('path')
+const path = require("path");
 
 const buildEslintCommand = (filenames) =>
-    `next lint --fix --file ${filenames
-        .map((f) => path.relative(process.cwd(), f))
-        .join(' --file ')}`
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`;
 
 module.exports = {
-    'src/*.{js,jsx,ts,tsx}': ["prettier --write --ignore-unknown", buildEslintCommand],
-}
+  "src/*.{js,jsx,ts,tsx}": [buildEslintCommand],
+  "**/*": ["prettier --write --ignore-unknown"],
+};
