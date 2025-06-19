dbf682be39aa28f61134d1a282bf492ffdf2f8e5
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-06-19 05:39:07 -0400

    Add error toasts for accessing restricted nav items

--- src/components/dock.tsx
+++ src/components/dock.tsx
@@ -4,45 +4,68 @@ import { usePathname } from "next/navigation";
 import { CirclePlus, CircleUserRound, Compass, LogIn } from "lucide-react";
 import Link from "next/link";
 import { useAuth } from "@/context/auth-provider";
+import React, { useState } from "react";
 
 export default function Dock() {
   const pathname = usePathname();
   const { session } = useAuth();
 
+  const [showToast, setShowToast] = useState(false);
+
+  function triggerToast(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    e.preventDefault();
+    setShowToast(true);
+    setTimeout(() => setShowToast(false), 5000);
+  }
+
   return (
-    <div className="dock sm:hidden">
-      <Link href="/" className={pathname === "/discover" ? "dock-active" : ""}>
-        <Compass className="size-[1.2em]" />
-        <span className="dock-label">Discover</span>
-      </Link>
-
-      <Link
-        href="/create"
-        className={pathname === "/create" ? "dock-active" : ""}
-      >
-        <CirclePlus className="size-[1.2em]" />
-        <span className="dock-label">Create</span>
-      </Link>
-
-      {session ? (
-        <Link
-          href="/users/me"
-          className={
-            pathname === `/users/${session?.user.id}` ? "dock-active" : ""
-          }
-        >
-          <CircleUserRound className="size-[1.2em]" />
-          <span className="dock-label">Profile</span>
-        </Link>
-      ) : (
-        <Link
-          href="/login"
-          className={pathname === `/login` ? "dock-active" : ""}
-        >
-          <LogIn className="size-[1.2em]" />
-          <span className="dock-label">Login</span>
-        </Link>
+    <>
+      {showToast && (
+        <div className="toast toast-top toast-center z-50">
+          <div className="alert alert-error">
+            <span>Log in to use this feature</span>
+          </div>
+        </div>
       )}
-    </div>
+      <div className="dock sm:hidden">
+        <Link
+          href="/"
+          className={pathname === "/discover" ? "dock-active" : ""}
+        >
+          <Compass className="size-[1.2em]" />
+          <span className="dock-label">Discover</span>
+        </Link>
+        <Link
+          href="/create"
+          className={pathname === "/create" ? "dock-active" : ""}
+          onClick={(e) => {
+            triggerToast(e);
+          }}
+        >
+          <CirclePlus className="size-[1.2em]" />
+          <span className="dock-label">Create</span>
+        </Link>
+
+        {session ? (
+          <Link
+            href="/users/me"
+            className={
+              pathname === `/users/${session?.user.id}` ? "dock-active" : ""
+            }
+          >
+            <CircleUserRound className="size-[1.2em]" />
+            <span className="dock-label">Profile</span>
+          </Link>
+        ) : (
+          <Link
+            href="/login"
+            className={pathname === `/login` ? "dock-active" : ""}
+          >
+            <LogIn className="size-[1.2em]" />
+            <span className="dock-label">Login</span>
+          </Link>
+        )}
+      </div>
+    </>
   );
 }
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -4,63 +4,86 @@ import { useAuth } from "@/context/auth-provider";
 import { CirclePlus, CircleUserRound, Compass, LogIn } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function Nav() {
   const pathname = usePathname();
   const { session } = useAuth();
 
+  const [showToast, setShowToast] = useState(false);
+
+  function triggerToast(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    e.preventDefault();
+    setShowToast(true);
+    setTimeout(() => setShowToast(false), 5000);
+  }
+
   return (
-    <div className="navbar hidden shadow-sm sm:flex">
-      <div className="navbar-start">
-        <Link href="/" className="btn btn-ghost text-xl">
-          TTG
-        </Link>
+    <>
+      {showToast && (
+        <div className="toast toast-top toast-center z-50">
+          <div className="alert alert-error">
+            <span>Log in to use this feature</span>
+          </div>
+        </div>
+      )}
+      <div className="navbar hidden shadow-sm sm:flex">
+        <div className="navbar-start">
+          <Link href="/" className="btn btn-ghost text-xl">
+            TTG
+          </Link>
+        </div>
+        <nav className="navbar-end hidden justify-end sm:flex">
+          <ul className="menu menu-horizontal gap-4">
+            <li>
+              <Link
+                href="/discover"
+                className={pathname === "/discover" ? "menu-active" : ""}
+              >
+                <Compass className="size-[1.2em]" />
+                Discover
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/create"
+                className={pathname === "/create" ? "menu-active" : ""}
+                onClick={(e) => {
+                  triggerToast(e);
+                }}
+              >
+                <CirclePlus className="size-[1.2em]" />
+                Create
+              </Link>
+            </li>
+            {session ? (
+              <li>
+                <Link
+                  href="/users/me"
+                  className={
+                    pathname === `/users/${session?.user.id}`
+                      ? "menu-active"
+                      : ""
+                  }
+                >
+                  <CircleUserRound className="size-[1.2em]" />
+                  Profile
+                </Link>
+              </li>
+            ) : (
+              <li>
+                <Link
+                  href="/login"
+                  className={pathname === "/login" ? "menu-active" : ""}
+                >
+                  <LogIn className="size-[1.2em]" />
+                  Login
+                </Link>
+              </li>
+            )}
+          </ul>
+        </nav>
       </div>
-      <nav className="navbar-end hidden justify-end sm:flex">
-        <ul className="menu menu-horizontal gap-4">
-          <li>
-            <Link
-              href="/discover"
-              className={pathname === "/discover" ? "menu-active" : ""}
-            >
-              <Compass className="size-[1.2em]" />
-              Discover
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/create"
-              className={pathname === "/create" ? "menu-active" : ""}
-            >
-              <CirclePlus className="size-[1.2em]" />
-              Create
-            </Link>
-          </li>
-          {session ? (
-            <li>
-              <Link
-                href="/users/me"
-                className={
-                  pathname === `/users/${session?.user.id}` ? "menu-active" : ""
-                }
-              >
-                <CircleUserRound className="size-[1.2em]" />
-                Profile
-              </Link>
-            </li>
-          ) : (
-            <li>
-              <Link
-                href="/login"
-                className={pathname === "/login" ? "menu-active" : ""}
-              >
-                <LogIn className="size-[1.2em]" />
-                Login
-              </Link>
-            </li>
-          )}
-        </ul>
-      </nav>
-    </div>
+    </>
   );
 }
