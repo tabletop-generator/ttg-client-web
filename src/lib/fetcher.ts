0d3012fe20784a9dd1006998af5334eb6ce48b73
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-06-25 14:51:16 -0400

    Improve fetcher error handling

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -18,12 +18,11 @@ export async function fetcher<T>([route, token]: [
   // If the status code is not in the range 200-299,
   // we still try to parse and throw it.
   if (!res.ok) {
-    const error = new Error(
-      `An error occurred while fetching the data.`,
-    ) as SWRResError;
     // Attach extra info to the error object.
-    error.info = await res.json();
-    error.status = res.status;
+    const error: SWRResError = Object.assign(
+      new Error(`An error occurred while fetching the data.`),
+      { info: await res.json(), status: res.status },
+    );
     throw error;
   }
 
