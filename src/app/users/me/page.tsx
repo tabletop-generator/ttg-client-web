9ebdd22e90493eec647da5158307894970991411
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-06-25 20:16:17 -0400

    Whitespace

--- src/app/users/me/page.tsx
+++ src/app/users/me/page.tsx
@@ -5,6 +5,7 @@ export default async function MePage() {
   const supabase = await createClient();
 
   const { data, error } = await supabase.auth.getUser();
+
   if (error || !data?.user) {
     redirect("/login");
   }
