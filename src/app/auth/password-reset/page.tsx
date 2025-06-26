1cd633b86c0737f885e1564685db33b7550ba23e
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-06-25 20:43:50 -0400

    Format password reset form

--- src/app/auth/password-reset/page.tsx
+++ src/app/auth/password-reset/page.tsx
@@ -3,7 +3,11 @@ import PasswordResetForm from "@/components/password-reset-form";
 export default async function PasswordResetPage() {
   return (
     <div className="flex grow items-center justify-center">
-      <PasswordResetForm />
+      <div className="m-6 w-96">
+        <div className="bg-base-100 border-base-300 p-6">
+          <PasswordResetForm />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/password-reset-form.tsx
+++ src/components/password-reset-form.tsx
@@ -35,7 +35,7 @@ export default function PasswordResetForm() {
         router.replace("/login");
       }
     });
-  });
+  }, [router]);
 
   async function onSubmit(data: PasswordResetInputs) {
     setLoading(true);
