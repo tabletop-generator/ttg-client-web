558ed2f73cdeced41238f8ef55325731dc029c7b
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-06-28 12:04:23 -0400

    Create asset card

--- src/components/asset-card.tsx
+++ src/components/asset-card.tsx
@@ -1,4 +1,6 @@
 import type { operations } from "@/types/schema";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function AssetCard({
   asset,
@@ -6,9 +8,30 @@ export default function AssetCard({
   asset: operations["getAssets"]["responses"]["200"]["content"]["application/json"]["assets"][0];
 }) {
   return (
-    <>
-      <div className="card bg-base-200 h-64" />
-      <p className="hidden">{JSON.stringify(asset)}</p>
-    </>
+    <Link href={`/assets/${asset.assetId}`}>
+      <div className="card bg-base-100 image-full group flex h-64 overflow-hidden">
+        <figure>
+          <Image
+            src={asset.imageUrl}
+            alt={`Image for the asset "${asset.name}"`}
+            fill
+            className="!brightness-100 group-hover:!brightness-50"
+          />
+        </figure>
+        <div className="card-body justify-end opacity-0 group-hover:opacity-100">
+          <h2 className="card-title truncate">{asset.name}</h2>
+          <div className="flex justify-between text-sm">
+            <span>
+              {asset.assetType.charAt(0).toUpperCase() +
+                asset.assetType.slice(1)}
+            </span>
+            <div className="flex gap-4">
+              <span>{asset.likeCount} likes</span>
+              <span>{asset.commentCount} comments</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
   );
 }
