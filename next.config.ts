f1c6d4969faa1b3a318b7f9e4b1be66c79a9312f
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-06-28 11:15:02 -0400

    Create collection card

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "3f14b9c99c9820d0eb18d1cbf3310f28.r2.cloudflarestorage.com",
+        pathname: "/ttg/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/components/collection-card.tsx
+++ src/components/collection-card.tsx
@@ -1,4 +1,7 @@
 import type { operations } from "@/types/schema";
+import { Layers2 } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function CollectionCard({
   collection,
@@ -6,9 +9,27 @@ export default function CollectionCard({
   collection: operations["getCollections"]["responses"]["200"]["content"]["application/json"]["collections"][0];
 }) {
   return (
-    <>
-      <div className="card bg-base-200 h-56 w-64 flex-shrink-0" />
-      <p className="hidden">{JSON.stringify(collection)}</p>
-    </>
+    <Link href={`/collections/${collection.collectionId}`}>
+      <div className="card bg-base-100 image-full group flex h-56 w-64 flex-col overflow-hidden">
+        {collection.coverImageUrl && (
+          <figure>
+            <Image
+              src={collection.coverImageUrl}
+              alt={`Cover image for the collection ${collection.name}`}
+              fill
+              className="!brightness-60 group-hover:!brightness-50"
+            />
+          </figure>
+        )}
+        <div className="card-body relative">
+          <h2 className="card-title flex-1 items-center justify-center truncate">
+            {collection.name}
+          </h2>
+          <div className="card-actions absolute right-4 bottom-4 flex items-center">
+            <Layers2 size={20} /> {collection.assetCount} assets
+          </div>
+        </div>
+      </div>
+    </Link>
   );
 }
