6883721bab0f7dfc4cb1d55deecfd2fd65a1522a
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-06-29 04:20:03 -0400

    Add return type to updateCurrentUser

--- src/lib/update-current-user.ts
+++ src/lib/update-current-user.ts
@@ -1,9 +1,12 @@
 import type { ResponseError } from "@/types/res-error";
+import type { operations } from "@/types/schema";
 
 export async function updateCurrentUser(
   data: { displayName?: string; bio?: string },
   token: string,
-) {
+): Promise<
+  operations["patchCurrentUser"]["responses"]["200"]["content"]["application/json"]
+> {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/v1/users/me`, {
     method: "PATCH",
     headers: {
@@ -21,5 +24,7 @@ export async function updateCurrentUser(
     throw error;
   }
 
-  return res.json();
+  return res.json() as Promise<
+    operations["patchCurrentUser"]["responses"]["200"]["content"]["application/json"]
+  >;
 }
